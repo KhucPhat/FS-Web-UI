a4bc29116307d087ff7435084d1651f0215dc96e
Phong <vanphong02062002@gmail.com>
2024-01-15 22:46:27 +0700

    fixed

---
+++ src/pages/SendForgotSuccess/SendForgotSuccess.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SuccessPage(): JSX.Element {
+  return (
+    <div>
+      <h2>Email sent successfully!</h2>
+      <p>Please check your email for further instructions.</p>
+    </div>
+  );
+}
